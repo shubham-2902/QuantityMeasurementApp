5d80c444db61b8367ce33638aa036092bda58180
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-02-23 16:29:02 +0530

    [Shubham Kumar Jha]: Added Test Cases for UC-6 Addition of Two Length Units

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -173,4 +173,20 @@ public class QuantityMeasurementAppTest {
                         .demonstrateLengthEquality(result, expected)
         );
     }
+    @Test
+    public void addFeetAndInches() {
+
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+
+        Length sumLength =
+                QuantityMeasurementApp.demonstrateLengthAddition(length1, length2);
+
+        Length expectedLength =
+                new Length(2.0, Length.LengthUnit.FEET);
+
+        assertTrue(
+            QuantityMeasurementApp.demonstrateLengthEquality(sumLength, expectedLength)
+        );
+    }
 }
