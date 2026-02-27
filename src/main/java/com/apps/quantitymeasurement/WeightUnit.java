081e1e6e0ce053ac086d915ed8995beecd23c6eb
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-02-27 16:02:34 +0530

    [Shubham Kumar Jha]: Updated the WeightUnit.java class for UC10:For generic class

--- src/main/java/com/apps/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -1,29 +1,31 @@
 package com.apps.quantitymeasurement;
 
-public enum WeightUnit {
+public enum WeightUnit implements IMeasurable {
 
     MILLIGRAM(0.001),
     GRAM(1.0),
     KILOGRAM(1000.0),
-    POUND(453.592);
+    POUND(453.592),
+    TONNE(1_000_000.0);
 
-    private final double conversionFactor; // to base unit (grams)
+    private final double conversionFactor;
 
     WeightUnit(double conversionFactor) {
         this.conversionFactor = conversionFactor;
     }
 
+    @Override
     public double getConversionFactor() {
         return conversionFactor;
     }
 
-    // Convert value in this unit → grams (base unit)
+    @Override
     public double convertToBaseUnit(double value) {
         return value * conversionFactor;
     }
 
-    // Convert grams → this unit
+    @Override
     public double convertFromBaseUnit(double baseValue) {
-        return baseValue / conversionFactor;
+        return Math.round((baseValue / conversionFactor) * 100.0) / 100.0;
     }
 }
