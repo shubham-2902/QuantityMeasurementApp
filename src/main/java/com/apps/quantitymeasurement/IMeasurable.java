5ee468080da7262d4538db4195465f136b49cc10
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-02-27 16:04:40 +0530

    [Shubham Kumar Jha]: Added the IMeasurable inteface for UC10:For generic class

---
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -0,0 +1,10 @@
+package com.apps.quantitymeasurement;
+
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+}
