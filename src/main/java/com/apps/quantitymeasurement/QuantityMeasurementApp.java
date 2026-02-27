4c642dfd809e333d9389c057e5dd72b85208121d
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-02-27 15:00:33 +0530

    [Shubham Kumar Jha]: Updated QuantityMeasurementApp class for UC9:Weight Measurement

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -48,6 +48,48 @@ public class QuantityMeasurementApp {
 
         return l1.add(l2, targetUnit);
     }
+    
+
+        public static boolean demonstrateWeightEquality(Weight w1, Weight w2) {
+            return w1.equals(w2);
+        }
+
+        public static boolean demonstrateWeightComparison(
+                double value1, WeightUnit unit1,
+                double value2, WeightUnit unit2) {
+
+            Weight w1 = new Weight(value1, unit1);
+            Weight w2 = new Weight(value2, unit2);
+
+            return w1.equals(w2);
+        }
+
+        public static Weight demonstrateWeightConversion(
+                double value,
+                WeightUnit fromUnit,
+                WeightUnit toUnit) {
+
+            Weight w = new Weight(value, fromUnit);
+            return w.convertTo(toUnit);
+        }
+
+        public static Weight demonstrateWeightConversion(
+                Weight weight,
+                WeightUnit toUnit) {
+
+            return weight.convertTo(toUnit);
+        }
+
+        public static Weight demonstrateWeightAddition(Weight w1, Weight w2) {
+            return w1.add(w2);
+        }
+
+        public static Weight demonstrateWeightAddition(
+                Weight w1, Weight w2, WeightUnit targetUnit) {
+
+            return w1.add(w2, targetUnit);
+        }
+    
 
     public static void main(String[] args) {
 
@@ -56,7 +98,16 @@ public class QuantityMeasurementApp {
 
         Length result = demonstrateLengthAddition(
                 l1, l2, LengthUnit.FEET);
+        
+        Weight w1 = new Weight(1.0, WeightUnit.KILOGRAM);
+        Weight w2 = new Weight(1000.0, WeightUnit.GRAM);
+        
+        Weight answer = demonstrateWeightAddition(w1, w2, WeightUnit.KILOGRAM);
+        
+
 
         System.out.println("Result = " + result);
+        System.out.println("Result = " + answer);
+        
     }
 }
