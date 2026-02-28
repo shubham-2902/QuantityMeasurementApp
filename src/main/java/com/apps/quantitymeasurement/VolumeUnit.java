c55fb2b20764cb109b1bf1a82a0027552fcaab59
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-02-28 13:51:58 +0530

    [Shubham Kumar Jha]: Added the new unit for Volume Measurement forUC11-Volume-Measurement

---
+++ src/main/java/com/apps/quantitymeasurement/VolumeUnit.java
@@ -0,0 +1,29 @@
+package com.apps.quantitymeasurement;
+
+public enum VolumeUnit implements IMeasurable {
+
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
+
+    private final double conversionFactor;
+
+    VolumeUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    @Override
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
