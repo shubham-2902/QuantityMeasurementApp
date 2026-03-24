9d9cb000e2f2748bc9c9d7cfe48c7bada624d384
Shubham Kumar Jha <shubhamjha4444@gmail.com>
2026-03-24 12:55:39 +0530

    [Shubham Kumar Jha]: Added Spring Backend for Quantity Measurement UC17

---
+++ src/main/java/com/app/quantitymeasurement/dto/QuantityInputDTO.java
@@ -0,0 +1,9 @@
+package com.app.quantitymeasurement.dto;
+
+import lombok.Data;
+
+@Data
+public class QuantityInputDTO {
+	private QuantityDTO thisQuantityDTO;
+	private QuantityDTO thatQuantityDTO;
+}
